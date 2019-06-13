d6e28d35b29190d4937a2eb568ac39b089e533f7
Justin D <justin@baal-software.com>
2019-06-13 09:45:20 -0600

    Init

---
+++ test/custom_functions/core_test.clj
@@ -0,0 +1,7 @@
+(ns custom-functions.core-test
+  (:require [clojure.test :refer :all]
+            [custom-functions.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
