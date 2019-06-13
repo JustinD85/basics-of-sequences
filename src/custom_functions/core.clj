7e9e08dd74e2b390b5165d638420b4ff4a6af76d
Justin D <justin@baal-software.com>
2019-06-13 10:20:29 -0600

    Corrects typos..

--- src/custom_functions/core.clj
+++ src/custom_functions/core.clj
@@ -12,11 +12,11 @@
 ;; and `cons` is considered to implement the sequence abstraction
 
 ;; I will implement a simple linked list of nodes to show understanding
-(def node-1 {:value "Mary" :next: node-2})
-(def node-2 {:value "had" :next: node-3})
-(def node-3 {:value "a" :next: node-4})
-(def node-4 {:value "little" :next: node-5})
-(def node-5 {:value "lamb" :next: null})
+(def node-5 {:value "lamb" :next nil})
+(def node-4 {:value "little" :next node-5})
+(def node-3 {:value "a" :next node-4})
+(def node-2 {:value "had" :next node-3})
+(def node-1 {:value "Mary" :next node-2})
 
 ;; I implement the three core functions that all sequences should respond to
 ;; NOTE:: this is modified to respond to maps, ordinarily a tuple would be returned
